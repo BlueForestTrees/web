d9a712c39d8c854ad420561da06d3d61d9f85c02
slimane <smedini@gmail.com>
2017-11-23 14:22:30 +0100

    web => web_old

--- src/js/domain/model/QtUnitShortName.js
+++
@@ -1,9 +0,0 @@
-export default class QtUnitShortName {
-    qt: number;
-    unitShortName: string;
-
-    constructor(qt:number, unitShortName:string) {
-        this.qt = qt;
-        this.unitShortName = unitShortName;
-    }
-}
--- src/js/domain/unit/Unit.js
+++
@@ -1,6 +0,0 @@
-export default class Unit {
-    grandeur: string;
-    shortName: string;
-    name: string;
-    coef: number;
-}
