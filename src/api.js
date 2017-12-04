90ec44054e7cd08f45a8be5c523a909327811d25
slimane <smedini@gmail.com>
2017-12-04 19:46:06 +0100

    web vue js tests

--- src/api.js
+++ src/api.js
@@ -1,20 +1,63 @@
 const req = require('request-promise-lite');
 const baseUrl = window.location.href;
 const api = {};
+
 const url = (path) => {
     return baseUrl + path;
 };
+
+const options = (body) => {
+    return {body: body, json: true};
+};
+
 const get = (path) => {
     return req.get(url(path), {json: true});
 };
 
+const post = (path, body) => {
+    return req.post(url(path), options(body));
+};
 
-
-
-api.headers = () => {
-    return get('api/headers');
+const del = (path) => {
+    return req.del(url(path));
 };
 
 
+api.all = () => {
+    return get('api/all');
+};
+
+api.purge = () => {
+    return get('adminapi/purge');
+};
+
+api.get = (_id, qt, unit) => {
+    if (unit)
+        return get(`api/trunk/${_id}/${qt}/${unit}`);
+    else if (qt)
+        return get(`api/trunk/${_id}/${qt}`);
+    else
+        return get(`api/trunk/${_id}`);
+};
+
+api.create = (trunk) => {
+    return post('api/trunk', trunk);
+};
+
+api.del = (trunkId) => {
+    return del(`api/trunk/${trunkId}`);
+};
+
+api.link = (link) => {
+    return post('api/root', link);
+};
+
+api.unlink = (trunkId, rootId) => {
+    return del(`api/root/${trunkId}/${rootId}`);
+};
+
+api.search = (namepart) => {
+    return get(`api/search/${namepart}`);
+};
 
 module.exports = api;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const api = require('api');
 
 (async () => {
-        console.log(await api.headers());
+
+        const ble = await api.create({name:"blé",qt:1,unit:'kg'});
+        const farine = await api.create({name:"farine",qt:1,unit:'kg'});
+        await api.link({trunkId:farine._id,rootId:ble._id,trunkQt:1,rootQt:1,unit:"kg"});
+        await api.get(farine._id,3,'g');
+
+        console.log(await api.purge());
 })();
