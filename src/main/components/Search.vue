c35921554b2595c2e14275e447037945b51e9954
slimane <smedini@gmail.com>
2017-12-30 16:20:56 +0100

    focus sur search bar

--- src/main/components/Search.vue
+++ src/main/components/Search.vue
@@ -1,11 +1,9 @@
 <template>
-    <v-menu offset-y style="width: 100%">
+    <v-menu offset-y style="width: 100%" ref="menu">
 
-        <v-text-field @input="termInputChanged" :value="search.term"
-                      slot="activator"
-                      light solo prepend-icon="search" placeholder="Recherche"/>
+        <v-text-field ref="textfield" @input="termInputChanged" :value="search.term" slot="activator" light solo prepend-icon="search" placeholder="Recherche"/>
 
-        <v-list v-if="search.results" focus>
+        <v-list v-if="search.results">
             <v-list-tile v-for="trunk in search.results" :key="trunk._id" @click="">
                 <v-list-tile-avatar>
                     <v-icon>assignment</v-icon>
@@ -15,7 +13,7 @@
             <v-divider v-if="allowCreate && hasResults"/>
             <v-list-tile v-if="allowCreate" @click="onCreateClick">
                 <v-list-tile-avatar>
-                    <v-icon >add</v-icon>
+                    <v-icon>add</v-icon>
                 </v-list-tile-avatar>
                 <v-list-tile-title>Créer "{{search.term}}"...</v-list-tile-title>
             </v-list-tile>
@@ -38,14 +36,21 @@
                 'termInputChanged': On.UPDATE_SEARCH_TERM
             }),
             ...mapMutations([Do.OPEN_CREATE_DIALOG, Do.CLEAR_SEARCH]),
-            onCreateClick:function(){
-                let creation = {name:this.search.term};
+            onCreateClick: function () {
+                let creation = {name: this.search.term};
                 this.clearSearch();
                 this.openCreateDialog(creation);
+            },
+            focus() {
+                this.$refs.textfield.focus();
+                this.$refs.menu.isActive = true;
             }
         },
         computed: {
             ...mapGetters(['allowCreate', 'hasResults'])
+        },
+        mounted() {
+            this.focus();
         }
     }
 </script>
