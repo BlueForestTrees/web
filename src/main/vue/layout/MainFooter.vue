addb2cf041c040af80b09bde43bb2cfae05a7764
slimane <smedini@gmail.com>
2018-02-05 10:02:18 +0100

    href useless

--- src/main/vue/layout/MainFooter.vue
+++ src/main/vue/layout/MainFooter.vue
@@ -2,8 +2,6 @@
     <v-footer color="blue darken-3" class="white--text" app>
         <span>Blueforest Trees</span>
         <a class="github-button" href="https://github.com/slimee/trees" data-size="large" aria-label="Participer sur GitHub">Participer sur GitHub</a>
-        <v-spacer></v-spacer>
-        <href a="http://github.com/slimee/trees"></href>
     </v-footer>
 </template>
 
