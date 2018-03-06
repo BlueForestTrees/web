5b3d1d3fd86258f29281f805ae71faaa0bbf7439
Clement <clement.langendorf@gmail.com>
2018-03-06 15:46:08 +0100

    Modif entrée qté 2

--- src/main/vue/common/InplaceEdit.vue
+++ src/main/vue/common/InplaceEdit.vue
@@ -2,14 +2,14 @@
     <span v-if="!editing" @click="edit">{{initial || 'qt?'}}</span>
     <v-layout v-else row>
         <v-form v-model="valid">
-        <v-text-field autofocus
-                      :value="initial"
-                      @input="current = $event" @keydown.esc.stop.native="ko" @keydown.enter.stop.native="ok"
-                      @blur="ko" :rules = "[rules.required,rules.isNumber]"
-        />
+            <v-text-field autofocus
+                          :value="initial"
+                          @input="current = $event" @keydown.esc.stop.native="ko" @keydown.enter.stop.native="ok"
+                          @blur="ko" :rules="[required,isNumber]"
+            />
         </v-form>
-                      <!--@blur="ko"-->
-        <v-icon  right :disabled="valid" color="green" @click="ok">done</v-icon>
+        <!--@blur="ko"-->
+        <v-icon right color="green" @click="ok">done</v-icon>
         <v-icon right color="red" @click="ko">clear</v-icon>
     </v-layout>
 </template>
@@ -23,20 +23,12 @@
             return {
                 editing: false,
                 current: this.initial,
-                valid: false,
-                rules: {
-                    required: (value) => !!value || 'Required.',
-                    isNumber: (value) => {
-                        const pattern = /^(\d*\.)?\d+$/
-                        console.log(value,pattern.test(value));
-                        return pattern.test(value) || 'Entrer un nombre.'
-                    }
-
-                }
+                valid: false
             }
         },
+
         methods: {
-            edit:function(){
+            edit: function () {
                 this.editing = true;
             },
             ok: function () {
@@ -47,8 +39,16 @@
                 this.editing = false;
                 this.current = this.initial;
                 this.$emit("ko");
+            },
+
+            required: (value) => !!value || 'Required.',
+            isNumber: (value) => {
+                const pattern = /^(\d*\.)?\d+$/
+                console.log(value, pattern.test(value))
+                return pattern.test(value) || 'Entrer un nombre.'
             }
         }
+
     }
 </script>
 
