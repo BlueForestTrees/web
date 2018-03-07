5011b2e22996236ea2aecd25f56721efd2b4625d
slimane <smedini@gmail.com>
2018-03-07 17:07:18 +0100

    getGrandeurs Ok

--- src/main/index.js
+++ src/main/index.js
@@ -1,30 +1,18 @@
-import initial from "./services/initialService";
-import initUnits from "./rest/unitInitial";
-
 import Vue from 'vue'
 import App from './vue/App'
 import store from './store/store'
 import Vuetify from 'vuetify'
-import _ from 'lodash';
+import rest from "./rest/routes";
+import {initUnits} from "trees-common/dist"
 
 Vue.config.productionTip = false;
 
 Promise
-    .all(initialCalls())
-    .then(fetchInitials)
+    .all([
+        rest.grandeurs()
+            .then(initUnits)
+    ])
     .then(initVue);
-
-function initialCalls() {
-    return [
-        initUnits()
-    ];
-}
-
-function fetchInitials(datas) {
-    _.forEach(datas, data => {
-        Object.assign(initial.unitModel, data);
-    });
-}
 
 function initVue() {
     Vue.use(Vuetify);
