f6e6509ecd79e950ec8b25d5cb738db7f01fc9fb
slim <smedini@gmail.com>
2018-08-10 17:06:53 +0200

    API Trunks OK

--- src/test/setup.js
+++ src/test/setup.js
@@ -7,7 +7,7 @@ export const init = async () => {
 
 export const withQuantity = (qt, unit) => ({quantity: {qt, unit}})
 export const withIdQuantity = (_id, qt, unit) => ({_id, ...withQuantity(qt, unit)})
-export const withNameIdQtGrandeur = (name, _id, qt, unit) => ({name, name_lower: name.toLowerCase(), ...withIdQuantity(_id, qt, unit), grandeur: grandeurOf(unit)})
+export const withNameIdQtGrandeur = (name, _id, qt, unit) => ({name, ...withIdQuantity(_id, qt, unit), grandeur: grandeurOf(unit)})
 const grandeurOf = unit => {
     switch (unit) {
         case 'kg':
