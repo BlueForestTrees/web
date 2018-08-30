fd6d7f5c27ca1053e085e3b36a5886d6702b9213
slim <smedini@gmail.com>
2018-08-30 18:26:58 +0200

    1.1.1 LOAD_TREE gère le needRefresh

--- src/main/store/actions/treeActions.js
+++ src/main/store/actions/treeActions.js
@@ -27,29 +27,29 @@ export default {
     },
     
     [On.LOAD_OPEN_TREE]: async ({state, dispatch, commit}, {_id, bqt}) => {
+        const tree = await dispatch(On.LOAD_TREE, {_id, bqt})
+        commit(Do.OPEN_TREE, tree)
+        return tree
+    },
+    
+    [On.LOAD_TREE]: async ({commit, state, dispatch}, {_id, bqt = 1}) => {
         const basketItem = state.basket[_id]
         let tree = null
         if (basketItem && !needRefresh(basketItem)) {
             tree = basketItem
         } else {
-            tree = await dispatch(On.LOAD_TREE, {_id, bqt})
+            tree = {_id}
+            tree.promises = {}
+            tree.promises.trunk = dispatch(On.LOAD_TRUNK, {_id, bqt}).then(trunk => Vue.set(tree, "trunk", trunk))
+            tree.promises.roots = dispatch(On.LOAD_ROOTS, {_id, bqt}).then(roots => Vue.set(tree, "roots", roots))
+            tree.promises.branches = dispatch(On.LOAD_BRANCHES, {_id, bqt}).then(branches => Vue.set(tree, "branches", branches))
+            tree.promises.impacts = dispatch(On.LOAD_IMPACTS, {_id, bqt}).then(impacts => Vue.set(tree, "impacts", impacts))
+            tree.promises.impactsTank = dispatch(On.LOAD_IMPACTS_TANK, {_id, bqt}).then(impactsTank => Vue.set(tree, "impactsTank", impactsTank))
+            tree.promises.all = Promise.all([tree.promises.trunk, tree.promises.roots, tree.promises.branches, tree.promises.impacts, tree.promises.impactsTank])
+            // dispatch(On.LOAD_TANK, treeToLoad)
+            // dispatch(On.LOAD_FACETS, treeToLoad)
+            dispatch(On.ADD_TO_BASKET, [tree])
         }
-        commit(Do.OPEN_TREE, tree)
-        return tree
-    },
-    
-    [On.LOAD_TREE]: ({commit, state, dispatch}, {_id, bqt = 1}) => {
-        const tree = {_id}
-        tree.promises = {}
-        tree.promises.trunk = dispatch(On.LOAD_TRUNK, {_id, bqt}).then(trunk => Vue.set(tree, "trunk", trunk))
-        tree.promises.roots = dispatch(On.LOAD_ROOTS, {_id, bqt}).then(roots => Vue.set(tree, "roots", roots))
-        tree.promises.branches = dispatch(On.LOAD_BRANCHES, {_id, bqt}).then(branches => Vue.set(tree, "branches", branches))
-        tree.promises.impacts = dispatch(On.LOAD_IMPACTS, {_id, bqt}).then(impacts => Vue.set(tree, "impacts", impacts))
-        tree.promises.impactsTank = dispatch(On.LOAD_IMPACTS_TANK, {_id, bqt}).then(impactsTank => Vue.set(tree, "impactsTank", impactsTank))
-        tree.promises.all = Promise.all([tree.promises.trunk, tree.promises.roots, tree.promises.branches, tree.promises.impacts, tree.promises.impactsTank])
-        // dispatch(On.LOAD_TANK, treeToLoad)
-        // dispatch(On.LOAD_FACETS, treeToLoad)
-        dispatch(On.ADD_TO_BASKET, [tree])
         return tree
     },
     
