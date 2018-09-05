84197dda82c9ae5a626d0250bafbfe7518239830
slim <smedini@gmail.com>
2018-09-05 16:53:20 +0200

    css

--- src/main/vue/compare/Compare.vue
+++ src/main/vue/compare/Compare.vue
@@ -1,5 +1,8 @@
 <template>
     <span>
+        <v-layout row wrap justify-center align-center class="ma-4">
+            <span class="title">Comparaison</span>
+        </v-layout>
         <compare-ribbon v-if="leftId && rightId" :axises="axises" :left="compare.left" :right="compare.right" :leftColor="leftColor" :rightColor="rightColor" />
         <v-card-text class="text-md-center" v-else-if="loading">Chargement...</v-card-text>
         <v-card-text class="text-md-center" v-else="!leftId">Faites une <span><v-icon @click="goSearch" color="blue">search</v-icon> recherche</span> ou prenez des produits du <span><v-icon @click="goBasket" color="blue">shopping_basket</v-icon> panier pour les comparer.</span></v-card-text>
